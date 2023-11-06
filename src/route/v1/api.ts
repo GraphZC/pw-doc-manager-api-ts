33345c9e37756d82e75e87bfcbc018fdbbd6f727
Tanaroeg O-Charoen <tanaroeg.o@ku.th>
2023-11-07 04:36:41 +0700

    Add auth middleware

--- src/route/v1/api.ts
+++ src/route/v1/api.ts
@@ -2,6 +2,7 @@ import { Router } from "express"
 import { healthCheck } from "@/controller/controller";
 import { login, refreshToken } from "@/controller/authController";
 import { getAllEmployees, updateEmployeeById, createEmployee , getEmployeeById, deleteEmployeeById } from "@/controller/employeeController";
+import authMiddleware from "@/middleware/authMiddleware";
 
 const apiRouter = (): Router => {
     const api = Router();
@@ -13,11 +14,11 @@ const apiRouter = (): Router => {
     api.post('/auth/refresh', refreshToken)
 
     // Employee
-    api.get('/employee', getAllEmployees);
-    api.get('/employee/:id', getEmployeeById);
-    api.post('/employee', createEmployee);
-    api.put('/employee/:id', updateEmployeeById);
-    api.delete('/employee/:id', deleteEmployeeById);
+    api.get('/employee', authMiddleware, getAllEmployees);
+    api.get('/employee/:id', authMiddleware, getEmployeeById);
+    api.post('/employee', authMiddleware, createEmployee);
+    api.put('/employee/:id', authMiddleware, updateEmployeeById);
+    api.delete('/employee/:id', authMiddleware, deleteEmployeeById);
 
     return api;
 }
